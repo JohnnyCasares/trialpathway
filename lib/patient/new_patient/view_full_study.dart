23fd666d3aebb6a6377919635d8ff6557293c608
Johnny Casares <88862676+JohnnyCasares@users.noreply.github.com>
2024-04-28 15:27:07 -0400

    used tables to display data in view_full_study.dart

--- lib/patient/new_patient/view_full_study.dart
+++ lib/patient/new_patient/view_full_study.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hti_trialpathway/class_models/clinical_trial.dart';
+import 'package:hti_trialpathway/class_models/clinical_trial_objects.dart';
 import 'package:hti_trialpathway/widgets/my_appbar.dart';
 import 'package:provider/provider.dart';
 
@@ -11,10 +12,10 @@ class ViewFullStudy extends StatelessWidget {
 
   final ClinicalTrial clinicalTrial;
 
-
   @override
   Widget build(BuildContext context) {
-    DatabaseQueries databaseQueries= DatabaseQueries(Provider.of<DBProvider>(context).getConnection());
+    DatabaseQueries databaseQueries =
+        DatabaseQueries(Provider.of<DBProvider>(context).getConnection());
     return Scaffold(
       appBar: const MyAppBar(),
       body: FutureBuilder(
@@ -31,19 +32,105 @@ class ViewFullStudy extends StatelessWidget {
                 ),
                 Text(clinicalTrial.detailedDescription ??
                     clinicalTrial.description),
-                Divider(),
-                Text(clinicalTrial.interventions?.length.toString() ?? 'Nada'),
-                Divider(),
-                Text(
-                    clinicalTrial.outcomeMeasures?.length.toString() ?? 'Nada'),
-                Divider(),
-                Text(clinicalTrial.contactInformation?.length.toString() ??
-                    'Nada'),
-                Divider(),
-                Text(clinicalTrial.contactsLocations?.length.toString() ??
-                    'Nada'),
-                Divider(),
-                Text(clinicalTrial.sponsors?.length.toString() ?? 'Nada'),
+                if (clinicalTrial.interventions != null &&
+                    clinicalTrial.interventions!.isNotEmpty)
+                  Card(
+                    child:
+                        ExpansionTile(title: const Text('Interventions'), children: [
+                      Table(
+                        children: [
+                          const TableRow(children: [
+                            Text('Type'),
+                            Text('Name'),
+                            Text('Description'),
+                          ]),
+                          ...clinicalTrial.interventions!
+                              .map((e) => intervention(e))
+                        ],
+                      )
+                    ]),
+                  ),
+                if (clinicalTrial.outcomeMeasures != null &&
+                    clinicalTrial.outcomeMeasures!.isNotEmpty)
+                  Card(
+                    child: ExpansionTile(
+                        title: const Text('Outcome Measures'),
+                        children: [
+                          Table(
+                            children: [
+                              const TableRow(children: [
+                                Text('Type'),
+                                Text('Description'),
+                              ]),
+                              ...clinicalTrial.outcomeMeasures!
+                                  .map((e) => outcome(e))
+                            ],
+                          )
+                        ]),
+                  ),
+                if (clinicalTrial.contactInformation != null &&
+                    clinicalTrial.contactInformation!.isNotEmpty)
+                  Card(
+                    child: ExpansionTile(
+                      title: const Text('Contact Information'),
+                      children: [
+                        Table(
+                          children: [
+                            const TableRow(children: [
+                              Text('Name'),
+                              Text('Phone'),
+                              Text('Email'),
+                            ]),
+                            ...clinicalTrial.contactInformation!
+                                .map((e) => contactInformation(e))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                if (clinicalTrial.contactsLocations != null &&
+                    clinicalTrial.contactsLocations!.isNotEmpty)
+                  Card(
+                    child: ExpansionTile(
+                      title: const Text('Locations and Contacts'),
+                      children: [
+                        Table(
+                          children: [
+                            const TableRow(children: [
+                              Text('Name'),
+                              Text('City'),
+                              Text('State'),
+                              Text('Zip'),
+                              Text('Country'),
+
+                            ]),
+                            ...clinicalTrial.contactsLocations!
+                                .map((e) => locationsAndContacts(e)
+                            )
+                          ],
+                        )
+                      ]
+                          ,
+                    ),
+                  ),
+                if (clinicalTrial.sponsors != null &&
+                    clinicalTrial.sponsors!.isNotEmpty)
+                  Card(
+                    child: ExpansionTile(
+                      title: const Text('Sponsors'),
+                      children: [
+                        Table(children: [
+                          const TableRow(children: [
+                            Text('Name'),
+                            Text('Agency Class'),
+                            Text('Type'),
+                          ]),
+                          ...clinicalTrial.sponsors!
+                              .map((e) => sponsors(e))
+                        ],)
+                          ]
+                    ),
+                  ),
               ],
             );
           } else {
@@ -54,6 +141,52 @@ class ViewFullStudy extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+
+  TableRow intervention(Intervention intervention) {
+    return TableRow(children: [
+      Text('${intervention.type}'),
+      Text('${intervention.name!}'),
+      Text('${intervention.description!}'),
+    ]);
+  }
+
+  TableRow outcome(Outcome outcome) {
+    return TableRow(children: [
+      Text('${outcome.outComeType!}'),
+      Text('${outcome.outComeDescription!}'),
+    ]);
+  }
+
+  TableRow contactInformation(ContactInformation contactInformation) {
+    return TableRow(children: [
+      Text('${contactInformation.name!}'),
+      Text('${contactInformation.phone!}'),
+      Text('${contactInformation.email!}'),
+    ]);
+  }
+
+  TableRow locationsAndContacts(ContactLocation contactLocation) {
+    return TableRow(
+      children: [
+        Text('${contactLocation.name}'),
+        Text('${contactLocation.city}'),
+        Text('${contactLocation.state}'),
+        Text('${contactLocation.zip}'),
+        Text('${contactLocation.country}'),
+      ]
+    );
+  }
+
+  TableRow sponsors(Sponsors sponsors) {
+
+    return TableRow(
+      children: [
+        Text('${sponsors.name}'),
+        Text('${sponsors.agencyClass}'),
+        Text('${sponsors.leadOrCollaborator}'),
+      ]
     );
   }
 }
